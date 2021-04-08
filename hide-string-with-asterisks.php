d577e9ef18fb84c083ab079d66ecbd6814c18c9f
Erik Sonni <42903306+5onn1@users.noreply.github.com>
2021-04-08 11:37:53 -0600

    create hide_digits functions

---
+++ hide-string-with-asterisks.php
@@ -0,0 +1,18 @@
+<?php
+
+# $data: The string to process.
+# $replacement: The replacement.
+# $visible_digits: Number of digits to be visible, in this case the lasts.
+
+function hide_digits($data, $replacement, $visible_digits) {
+  //return substr_replace($mobile, '*', 0, $data_length-4);
+  $data_length = strlen($data);
+  $rplc = $replacement;
+  for ($i = 1; $i <= ($data_length-$visible_digits)-1; $i++) {
+    $rplc = $rplc . $replacement;
+  }
+
+  return substr_replace($data, $rplc, 0, $data_length-$visible_digits);
+}
+ echo hide_digits("123456789", "*", 4);
+?>
